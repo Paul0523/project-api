8abd9aff30a899705b266852513993308cc29561
fangzhipeng <zhpfang@qiuzhitech.com>
2018-04-02 18:55:45 +0800

    增加部署脚本

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,6 @@ PROJECT_PATH=/home/ubuntu/web_service/projcect-web
 
 cd $PROJECT_PATH
 
-ps -ef |grep 'python application.py' |awk '{print $2}'|xargs kill -9
-
 git pull --rebase
 
-pyenv activate project
-
-nohup python application.py &
+./restart.sh
